4b42de3c927935e77b366c892cfed496e0c872a3
Bastian Inuk Christensen <bastian@inuk.blog>
2019-12-06 19:11:47 +0100

    opped min iOS version for compilability

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BookKit",
     platforms: [
-        .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_11), .iOS(.v10), .tvOS(.v9), .watchOS(.v2)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
