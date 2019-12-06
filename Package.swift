2a65de4a5ab8445c1982164ad4f02f74ecf493b9
Bastian Inuk Christensen <bastian@inuk.blog>
2019-12-06 18:53:18 +0100

    added min. platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "BookKit",
-    
+    platforms: [
+        .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
