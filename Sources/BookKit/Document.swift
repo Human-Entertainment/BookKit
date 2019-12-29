3faad86e727a79442044ad72bd5d43a411c45a6d
Bastian Inuk Christensen <bastian@inuk.blog>
2019-12-29 21:44:46 +0100

    Fixed some compilation errors with Document

--- Sources/BookKit/Document.swift
+++ Sources/BookKit/Document.swift
@@ -14,14 +14,10 @@ public class Document {
     public let fileURL: URL
     public init(fileURL: URL) {
         self.fileURL = fileURL
-        do {
-            data = try? Data(contentsOf: fileURL)
-        } catch {
-            throw DocumentError.FileNotFound
-        }
+        self.data = try? Data(contentsOf: fileURL)
     }
     
-    public func open(completionHandler: (Bool) -> (Void) = nil){
+    public func open(completionHandler: (Bool) -> (Void)?){
         completionHandler(data != nil ? true : false)
     }
 }
